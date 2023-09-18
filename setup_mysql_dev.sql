31b2aece9ccd213b2cce4a99b3ebc88aa678000d
yasmeen <yasmeenhany14@gmail.com>
2023-09-18 13:14:46 +0300

    give priv and remove single quotes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- script that sets up the data base for the project
 -- and sets up the user and it's privilages
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
