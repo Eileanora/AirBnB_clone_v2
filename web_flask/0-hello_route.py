39cb1f386aa36007e78b2c3daaa95413d1fcf9a5
yasmeen <yasmeenhany14@gmail.com>
2023-10-22 21:57:00 +0300

    added first line

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 '''script that starts a flask web application'''
 from flask import Flask
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     '''hello function'''
     return 'Hello HBNB!'
 
