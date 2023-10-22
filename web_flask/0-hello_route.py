c0a10e0a4560f539c0eb77d894a947d37e099304
yasmeen <yasmeenhany14@gmail.com>
2023-10-22 21:54:53 +0300

    first task

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+'''script that starts a flask web application'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''hello function'''
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0:5000')
