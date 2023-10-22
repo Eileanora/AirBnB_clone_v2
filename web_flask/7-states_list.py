75d77fa8ee5c24b98cdb8a34052c332e2b9a8ac0
yasmeen <yasmeenhany14@gmail.com>
2023-10-22 23:30:51 +0300

    fix data not showing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,9 +16,10 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     '''display a HTML page'''
-    states = storage.all('State')
+    states = storage.all('State').values()
+    states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
